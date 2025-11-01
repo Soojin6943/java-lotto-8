2da4c4846184bc6473fc5a234c10847fc9d3a211
Soojin6943 <sjhan0711@gmail.com>
2025-11-01 18:43:39 +0900

    feat(view): 로또 갯숫 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.view.InputView;
+import lotto.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
@@ -11,16 +12,14 @@ public class Application {
             try {
                 // 문자열로 입력받은 구매 금액
                 String amountPrice = inputView.readAmountPrice();
-                System.out.println(amountPrice);
 
                 // 문자열 구매 금액 -> 정수 구매금액
                 int intPrice = parser.parseAmount(amountPrice);
-                System.out.println(intPrice);
 
                 // 구매 금액을 가격으로
                 Price price = new Price(intPrice);
                 int lottoCnt = price.toLottoCount();
-                System.out.println(lottoCnt);
+                OutputView.printLottoCount(lottoCnt);
                 return;
             } catch (IllegalArgumentException e) {
                 // TODO view로 변경하기
---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,7 @@
+package lotto.view;
+
+public class OutputView {
+    public static void printLottoCount(int lottoCnt) {
+        System.out.println(lottoCnt + "개를 구매했습니다.");
+    }
+}

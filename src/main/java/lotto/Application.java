4e3fb0d4e0847cedba6d0519f927c795c72f91f4
Soojin6943 <sjhan0711@gmail.com>
2025-11-01 15:48:42 +0900

    feat(price): 입력받은 로또 구매금액을 정수로 변환

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,8 +5,12 @@ import lotto.view.InputView;
 public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
+        Price price = new Price();
 
         String amountPrice = inputView.readAmountPrice();
         System.out.println(amountPrice);
+
+        int intPrice = price.parseAmount(amountPrice);
+        System.out.println(intPrice);
     }
 }
---
+++ src/main/java/lotto/Price.java
@@ -0,0 +1,12 @@
+package lotto;
+
+public class Price {
+
+    public int parseAmount(String amount) {
+        try {
+            return Integer.parseInt(amount.trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 구매 금액은 숫자여야 합니다.");
+        }
+    }
+}
